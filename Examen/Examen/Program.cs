3b016ce6ee7784aafc8e09529e02cc79747614e6
Facundo <facu910112@gmail.com>
2021-06-26 12:56:02 -0300

    cuato commit

--- Examen/Examen/Program.cs
+++ Examen/Examen/Program.cs
@@ -10,6 +10,37 @@ namespace Examen
     {
         static void Main(string[] args)
         {
+            Alumno Facu = new Alumno();
+            OpAbm op1 = new OpAbm(Facu);
+            OpBusqueda opb1 = new OpBusqueda(Facu);
+            Profesor Andres = new Profesor();
+            OpAbm op2 = new OpAbm(Andres);
+            OpBusqueda opb2 = new OpBusqueda(Andres);
+            Materia mat1 = new Materia();
+            OpAbm op3 = new OpAbm(mat1);
+
+
+            Console.WriteLine(op1.Altas());
+            Console.WriteLine(op1.Bajas());
+            Console.WriteLine(op1.Modificaciones());
+            Console.WriteLine(opb1.Busqueda());
+            Console.WriteLine(opb1.BusquedaDni());
+
+            Console.WriteLine(op2.Altas());
+            Console.WriteLine(op2.Bajas());
+            Console.WriteLine(op2.Modificaciones());
+            Console.WriteLine(opb2.Busqueda());
+            Console.WriteLine(opb2.BusquedaDni());
+
+            Console.WriteLine(op3.Altas());
+            Console.WriteLine(op3.Bajas());
+            Console.WriteLine(op3.Modificaciones());
+
+            Console.ReadLine();
+
+
+
+
         }
     }
 }
